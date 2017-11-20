2bfec35aa44ad99531a049cc7f130543847cd627
Eugene Skepner <github@skepner.eu>
2017-11-20 10:07:22 +0100

    hidb-maker development

--- cc/hidb-maker.cc
+++ cc/hidb-maker.cc
@@ -1,3 +1,5 @@
+#include "acmacs-base/enumerate.hh"
+#include "acmacs-base/virus-name.hh"
 #include "acmacs-chart-2/chart.hh"
 #include "hidb-maker.hh"
 
@@ -5,6 +7,14 @@
 
 void HidbMaker::add(const acmacs::chart::Chart& aChart)
 {
+    auto* table = mTables.add(aChart);
+    table->set_titers(*aChart.titers());
+    auto source_antigens = aChart.antigens();
+    for (auto source_antigen: *source_antigens) {
+        auto target_antigen = mAntigens.add(*source_antigen);
+        target_antigen->add_table(table);
+        table->add_antigen(target_antigen);
+    }
 
 } // HidbMaker::add
 
@@ -17,6 +27,127 @@ void HidbMaker::save(std::string aFilename)
 
 // ----------------------------------------------------------------------
 
+Table::Table(const acmacs::chart::Info& aInfo)
+    : virus(aInfo.virus()), virus_type(aInfo.virus_type()), subset(aInfo.subset()),
+      assay(aInfo.assay()), date(aInfo.date()), lab(aInfo.lab()), rbc_species(aInfo.rbc_species())
+{
+
+} // Table::Table
+
+// ----------------------------------------------------------------------
+
+void Table::set_titers(const acmacs::chart::Titers& aTiters)
+{
+    titers.resize(aTiters.number_of_antigens());
+    for (auto [ag_no, row]: acmacs::enumerate(titers)) {
+        row.resize(aTiters.number_of_sera());
+        for (auto [sr_no, target]: acmacs::enumerate(row)) {
+            target = aTiters.titer(ag_no, sr_no);
+        }
+    }
+
+} // Table::set_titers
+
+// ----------------------------------------------------------------------
+
+Table* Tables::add(const acmacs::chart::Chart& aChart)
+{
+    auto table{std::make_unique<Table>(*aChart.info())};
+    const auto insert_at = lower_bound(table);
+    if (insert_at != end() && **insert_at == *table)
+        throw std::runtime_error("Table " + acmacs::to_string(*table) + " is already in hidb");
+    std::cerr << "DEBUG: adding " << acmacs::to_string(*table) << '\n';
+    const auto inserted = insert(insert_at, std::move(table));
+    return inserted->get();
+
+} // Tables::add
+
+// ----------------------------------------------------------------------
+
+Antigen* Antigens::add(const acmacs::chart::Antigen& aAntigen)
+{
+    auto antigen{std::make_unique<Antigen>(aAntigen)};
+    auto insert_at = lower_bound(antigen);
+    if (insert_at == end() || **insert_at != *antigen) {
+        insert_at = insert(insert_at, std::move(antigen));
+        std::cerr << "DEBUG: AG " << (*insert_at)->to_string() << '\n';
+    }
+    (*insert_at)->add_date(aAntigen.date());
+    const auto lab_ids{aAntigen.lab_ids()};
+    (*insert_at)->add_lab_id(lab_ids.begin(), lab_ids.end());
+    return insert_at->get();
+
+} // Antigens::add
+
+// ----------------------------------------------------------------------
+
+AntigenSerum::~AntigenSerum()
+{
+} // AntigenSerum::~AntigenSerum
+
+// ----------------------------------------------------------------------
+
+void AntigenSerum::add_table(Table *aTable)
+{
+    if (auto found = table_ptrs.find(aTable); found != table_ptrs.end())
+        throw std::runtime_error(type_name() + " " + to_string() + " already in the table " + acmacs::to_string(*aTable) + ", duplicate?");
+    table_ptrs.insert(aTable);
+
+} // AntigenSerum::add_table
+
+// ----------------------------------------------------------------------
+
+Antigen::Antigen(const acmacs::chart::Antigen& aAntigen)
+    : reassortant{aAntigen.reassortant()}, passage{aAntigen.passage()}, annotations{aAntigen.annotations()},
+      lineage{aAntigen.lineage()}
+{
+    const std::string name{aAntigen.name()};
+    try {
+        std::string temp_passage;
+        virus_name::split(name, virus_type, host, location, isolation, year, temp_passage);
+    }
+    catch (virus_name::Unrecognized&) {
+        if (name.size() > 3 && (name[2] == ' ' || name[2] == '-')) {
+              // cdc name
+            location = name.substr(0, 2);
+            isolation = name.substr(3);
+        }
+        else
+            throw std::runtime_error("Unrecognized antigen name: " + name);
+    }
+
+} // Antigen::Antigen
+
+// ----------------------------------------------------------------------
+
+Serum::Serum(const acmacs::chart::Serum& aSerum)
+{
+
+} // Serum::Serum
+
+// ----------------------------------------------------------------------
+
+std::string acmacs::to_string(const Table& aTable)
+{
+    return string::join(":", {aTable.virus, aTable.virus_type, aTable.subset, aTable.assay, aTable.lab, aTable.rbc_species, aTable.date});
+
+} // acmacs::to_string
+
+// ----------------------------------------------------------------------
+
+std::string Antigen::to_string() const
+{
+    return string::join(" ", {string::join("/", {virus_type, host, location, isolation, year}), string::join(" ", annotations), reassortant, passage});
+
+} // Antigen::to_string
+
+// ----------------------------------------------------------------------
+
+std::string Serum::to_string() const
+{
+    return string::join(" ", {string::join("/", {virus_type, host, location, isolation, year}), string::join(" ", annotations), reassortant, serum_id});
+
+} // Serum::to_string
 
 // ----------------------------------------------------------------------
 /// Local Variables:
--- cc/hidb-maker.hh
+++ cc/hidb-maker.hh
@@ -1,11 +1,21 @@
 #pragma once
 
 #include <vector>
+#include <set>
+#include <functional>
 #include <memory>
 
 #include "acmacs-base/string.hh"
 
-namespace acmacs::chart { class Chart; }
+namespace acmacs::chart
+{
+    class Chart;
+    class Info;
+    class Titers;
+    class Antigen;
+    class Serum;
+
+} // namespace acmacs::chart
 
 // ----------------------------------------------------------------------
 
@@ -15,9 +25,43 @@ class Table;
 class Antigen;
 class Serum;
 
-using AntigenPtrs = std::vector<const Antigen*>;
-using SerumPtrs = std::vector<const Serum*>;
-using TablePtrs = std::vector<const Table*>;
+using AntigenPtrs = std::set<const Antigen*>;
+using SerumPtrs = std::set<const Serum*>;
+using TablePtrs = std::set<const Table*>;
+
+// ----------------------------------------------------------------------
+
+template <typename T> class less_unique_ptr
+{
+ public:
+    constexpr inline bool operator()(const std::unique_ptr<T>& lhs, const std::unique_ptr<T>& rhs) const
+        {
+            return *lhs < *rhs;
+        }
+
+}; // class less_unique_ptr<>
+
+template <typename T> class set_unique_ptr : public std::set<std::unique_ptr<T>, less_unique_ptr<T>>
+{
+}; // class set_unique_ptr <>
+
+// ----------------------------------------------------------------------
+
+class Virus : public std::string
+{
+ public:
+    inline Virus(std::string aSource) : std::string{aSource == "influenza" ? std::string{} : aSource} {}
+
+}; // class Virus
+
+// ----------------------------------------------------------------------
+
+class Lineage : public std::string
+{
+ public:
+    inline Lineage(std::string aSource) : std::string{aSource == "UNKNOWN" ? std::string{} : aSource} {}
+
+}; // class Lineage
 
 // ----------------------------------------------------------------------
 
@@ -30,7 +74,7 @@ class Titers : public std::vector<std::vector<std::string>>
 class Table
 {
  public:
-    std::string virus;
+    Virus virus;
     std::string virus_type;
     std::string subset;
     std::string assay;
@@ -44,6 +88,8 @@ class Table
     AntigenPtrs antigen_ptrs;
     SerumPtrs serum_ptrs;
 
+    Table(const acmacs::chart::Info& aInfo);
+
     inline bool operator==(const Table& rhs) const { return string::compare(
         {    virus,     virus_type,     subset,     assay,     lab,     rbc_species,     date},
         {rhs.virus, rhs.virus_type, rhs.subset, rhs.assay, rhs.lab, rhs.rbc_species, rhs.date}) == 0; }
@@ -52,14 +98,23 @@ class Table
         {    virus,     virus_type,     subset,     assay,     lab,     rbc_species,     date},
         {rhs.virus, rhs.virus_type, rhs.subset, rhs.assay, rhs.lab, rhs.rbc_species, rhs.date}) < 0; }
 
+    void set_titers(const acmacs::chart::Titers& aTiters);
+    inline void add_antigen(Antigen* aAntigen) { antigen_ptrs.insert(aAntigen); }
+    inline void add_serum(Serum* aSerum) { serum_ptrs.insert(aSerum); }
+
 }; // class Table
+
+namespace acmacs
+{
+    std::string to_string(const Table& aTable);
+}
 
 // ----------------------------------------------------------------------
 
-class Tables : public std::vector<std::unique_ptr<Table>>
+class Tables : public set_unique_ptr<Table>
 {
  public:
-    inline void sort() { std::sort(begin(), end(), [](const auto& a, const auto& b) -> bool { return *a < *b; }); }
+    Table* add(const acmacs::chart::Chart& aChart);
 
 }; // class Tables
 
@@ -71,19 +126,40 @@ class Annotations : public std::vector<std::string>
 
 // ----------------------------------------------------------------------
 
-class Dates : public std::vector<std::string>
+class Dates : public std::set<std::string>
 {
 }; // class Dates
 
 // ----------------------------------------------------------------------
 
-class LabIds : public std::vector<std::string>
+class LabIds : public std::set<std::string>
 {
 }; // class LabIds
 
 // ----------------------------------------------------------------------
 
-class Antigen
+class AntigenSerum
+{
+ public:
+    Indexes tables;
+    TablePtrs table_ptrs;
+
+    virtual ~AntigenSerum();
+    virtual std::string to_string() const = 0;
+    virtual std::string type_name() const = 0;
+
+    void add_table(Table *aTable);
+
+}; // class AntigenSerum
+
+namespace acmacs
+{
+    inline std::string to_string(const AntigenSerum& aAntigenSerum) { return aAntigenSerum.to_string(); }
+}
+
+// ----------------------------------------------------------------------
+
+class Antigen : public AntigenSerum
 {
  public:
     std::string virus_type;    // empty for cdc name
@@ -96,33 +172,39 @@ class Antigen
     Annotations annotations;
     Dates dates;
     LabIds lab_ids;
-    std::string lineage;
-    Indexes tables;
+    Lineage lineage;
 
-    TablePtrs table_ptrs;
+    Antigen(const acmacs::chart::Antigen& aAntigen);
 
     inline bool operator==(const Antigen& rhs) const { return string::compare(
         {    virus_type,     host,     location,     isolation,     year, string::join(" ",     annotations),     reassortant,     passage},
         {rhs.virus_type, rhs.host, rhs.location, rhs.isolation, rhs.year, string::join(" ", rhs.annotations), rhs.reassortant, rhs.passage}) == 0; }
+    inline bool operator!=(const Antigen& rhs) const { return !operator==(rhs); }
 
     inline bool operator<(const Antigen& rhs) const { return string::compare(
         {    virus_type,     host,     location,     isolation,     year, string::join(" ",     annotations),     reassortant,     passage},
         {rhs.virus_type, rhs.host, rhs.location, rhs.isolation, rhs.year, string::join(" ", rhs.annotations), rhs.reassortant, rhs.passage}) < 0; }
 
+    template <typename Iter> inline void add_lab_id(Iter first, Iter last) { for (; first != last; ++first) lab_ids.insert(*first); }
+    inline void add_date(std::string aSource) { if (!aSource.empty()) dates.insert(aSource); }
+
+    inline std::string type_name() const override { return "Antigen"; }
+    std::string to_string() const override;
+
 }; // class Antigen
 
 // ----------------------------------------------------------------------
 
-class Antigens : public std::vector<std::unique_ptr<Antigen>>
+class Antigens : public set_unique_ptr<Antigen>
 {
  public:
-    inline void sort() { std::sort(begin(), end(), [](const auto& a, const auto& b) -> bool { return *a < *b; }); }
+    Antigen* add(const acmacs::chart::Antigen& aAntigen);
 
 }; // class Antigens
 
 // ----------------------------------------------------------------------
 
-class Serum
+class Serum : public AntigenSerum
 {
  public:
     std::string virus_type;
@@ -136,26 +218,28 @@ class Serum
     std::string serum_id;
     std::string serum_sepcies;
     std::string lineage;
-    Indexes tables;
 
-    TablePtrs table_ptrs;
+    Serum(const acmacs::chart::Serum& aSerum);
 
     inline bool operator==(const Serum& rhs) const { return string::compare(
         {    virus_type,     host,     location,     isolation,     year, string::join(" ",     annotations),     reassortant,     serum_id},
         {rhs.virus_type, rhs.host, rhs.location, rhs.isolation, rhs.year, string::join(" ", rhs.annotations), rhs.reassortant, rhs.serum_id}) == 0; }
+    inline bool operator!=(const Serum& rhs) const { return !operator==(rhs); }
 
     inline bool operator<(const Serum& rhs) const { return string::compare(
         {    virus_type,     host,     location,     isolation,     year, string::join(" ",     annotations),     reassortant,     serum_id},
         {rhs.virus_type, rhs.host, rhs.location, rhs.isolation, rhs.year, string::join(" ", rhs.annotations), rhs.reassortant, rhs.serum_id}) < 0; }
 
+    inline std::string type_name() const override { return "Serum"; }
+    std::string to_string() const override;
+
 }; // class Serum
 
 // ----------------------------------------------------------------------
 
-class Sera : public std::vector<std::unique_ptr<Serum>>
+class Sera : public set_unique_ptr<Serum>
 {
  public:
-    inline void sort() { std::sort(begin(), end(), [](const auto& a, const auto& b) -> bool { return *a < *b; }); }
 
 }; // class Sera
 
