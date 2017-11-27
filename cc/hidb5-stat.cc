8dafe905506fed8642975776d8ab8d1564655dd9
Eugene Skepner <github@skepner.eu>
2017-11-27 12:23:37 +0100

    hidb-stat development

--- cc/hidb5-stat.cc
+++ cc/hidb5-stat.cc
@@ -5,6 +5,9 @@
 
 #include "acmacs-base/argc-argv.hh"
 #include "acmacs-base/string.hh"
+#include "acmacs-base/rjson.hh"
+#include "acmacs-base/read-file.hh"
+#include "acmacs-base/time.hh"
 #include "locationdb/locdb.hh"
 #include "hidb-5/hidb.hh"
 
@@ -13,8 +16,9 @@ using namespace std::string_literals;
 using data_key_t = std::tuple<std::string, std::string, std::string, std::string>; // virus_type, lab, date, continent
 using data_t = std::map<data_key_t, size_t>;
 
-static void make(std::string aStart, std::string aEnd);
+static void make(std::string aStart, std::string aEnd, std::string aFilename);
 static std::pair<data_t, data_t> scan(std::string aStart, std::string aEnd);
+static std::string make_json(const data_t& data_antigens, const data_t& data_sera);
 static std::string get_date(std::string aDate);
 
 // ----------------------------------------------------------------------
@@ -37,7 +41,7 @@ int main(int argc, char* const argv[])
         const bool verbose = args["-v"] || args["--verbose"];
         hidb::setup(args["--db-dir"], {}, verbose);
 
-        make(get_date(args["--start"]), get_date(args["--end"]));
+        make(get_date(args["--start"]), get_date(args["--end"]), args[0]);
 
         return 0;
     }
@@ -49,16 +53,17 @@ int main(int argc, char* const argv[])
 
 // ----------------------------------------------------------------------
 
-void make(std::string aStart, std::string aEnd)
+void make(std::string aStart, std::string aEnd, std::string aFilename)
 {
     auto [data_antigens, data_sera] = scan(aStart, aEnd);
+    acmacs::file::write(aFilename, make_json(data_antigens, data_sera));
 
     for (std::string virus_type: {"A(H1N1)", "A(H3N2)", "B"}) {
-        std::cout << std::setw(9) << std::left << virus_type << ": " << data_antigens[std::make_tuple(virus_type, "",  "",   "")] << '\n';
+        std::cout << std::setw(9) << std::left << virus_type << ": " << data_antigens[std::make_tuple(virus_type, "all",  "all",   "all")] << '\n';
         if (virus_type == "B") {
             for (std::string lineage: {"VICTORIA", "YAMAGATA", "UNKNOWN"}) {
                 auto vtl = virus_type + lineage;
-                std::cout << std::setw(9) << std::left << vtl << ": " << data_antigens[std::make_tuple(vtl, "",  "",   "")] << '\n';
+                std::cout << std::setw(9) << std::left << vtl << ": " << data_antigens[std::make_tuple(vtl, "all",  "all",   "all")] << '\n';
             }
         }
     }
@@ -70,6 +75,7 @@ void make(std::string aStart, std::string aEnd)
 std::pair<data_t, data_t> scan(std::string aStart, std::string aEnd)
 {
     auto& locdb = get_locdb();
+    const std::string all = "all";
 
     data_t data_antigens;
     std::string min_date{"3000"}, max_date{"1000"};
@@ -91,28 +97,28 @@ std::pair<data_t, data_t> scan(std::string aStart, std::string aEnd)
 
                 ++data_antigens[std::make_tuple(virus_type, lab, date, continent)];
                 ++data_antigens[std::make_tuple(virus_type, lab, year, continent)];
-                ++data_antigens[std::make_tuple(virus_type, lab, date, "")];
-                ++data_antigens[std::make_tuple(virus_type, lab, year, "")];
-                ++data_antigens[std::make_tuple(virus_type, lab, "",   continent)];
-                ++data_antigens[std::make_tuple(virus_type, lab, "",   "")];
-                ++data_antigens[std::make_tuple(virus_type, "",  date, continent)];
-                ++data_antigens[std::make_tuple(virus_type, "",  year, continent)];
-                ++data_antigens[std::make_tuple(virus_type, "",  date, "")];
-                ++data_antigens[std::make_tuple(virus_type, "",  year, "")];
-                ++data_antigens[std::make_tuple(virus_type, "",  "",   continent)];
-                ++data_antigens[std::make_tuple(virus_type, "",  "",   "")];
-                ++data_antigens[std::make_tuple("",         lab, date, continent)];
-                ++data_antigens[std::make_tuple("",         lab, year, continent)];
-                ++data_antigens[std::make_tuple("",         lab, date, "")];
-                ++data_antigens[std::make_tuple("",         lab, year, "")];
-                ++data_antigens[std::make_tuple("",         lab, "",   continent)];
-                ++data_antigens[std::make_tuple("",         lab, "",   "")];
-                ++data_antigens[std::make_tuple("",         "",  date, continent)];
-                ++data_antigens[std::make_tuple("",         "",  year, continent)];
-                ++data_antigens[std::make_tuple("",         "",  date, "")];
-                ++data_antigens[std::make_tuple("",         "",  year, "")];
-                ++data_antigens[std::make_tuple("",         "",  "",   continent)];
-                ++data_antigens[std::make_tuple("",         "",  "",   "")];
+                ++data_antigens[std::make_tuple(virus_type, lab, date, all)];
+                ++data_antigens[std::make_tuple(virus_type, lab, year, all)];
+                ++data_antigens[std::make_tuple(virus_type, lab, all,  continent)];
+                ++data_antigens[std::make_tuple(virus_type, lab, all,  all)];
+                ++data_antigens[std::make_tuple(virus_type, all, date, continent)];
+                ++data_antigens[std::make_tuple(virus_type, all, year, continent)];
+                ++data_antigens[std::make_tuple(virus_type, all, date, all)];
+                ++data_antigens[std::make_tuple(virus_type, all, year, all)];
+                ++data_antigens[std::make_tuple(virus_type, all, all,  continent)];
+                ++data_antigens[std::make_tuple(virus_type, all, all,  all)];
+                ++data_antigens[std::make_tuple(all,        lab, date, continent)];
+                ++data_antigens[std::make_tuple(all,        lab, year, continent)];
+                ++data_antigens[std::make_tuple(all,        lab, date, all)];
+                ++data_antigens[std::make_tuple(all,        lab, year, all)];
+                ++data_antigens[std::make_tuple(all,        lab, all,  continent)];
+                ++data_antigens[std::make_tuple(all,        lab, all,  all)];
+                ++data_antigens[std::make_tuple(all,        all, date, continent)];
+                ++data_antigens[std::make_tuple(all,        all, year, continent)];
+                ++data_antigens[std::make_tuple(all,        all, date, all)];
+                ++data_antigens[std::make_tuple(all,        all, year, all)];
+                ++data_antigens[std::make_tuple(all,        all, all,  continent)];
+                ++data_antigens[std::make_tuple(all,        all, all,  all)];
 
                 if (virus_type == "B") {
                     std::string vtl;
@@ -130,16 +136,16 @@ std::pair<data_t, data_t> scan(std::string aStart, std::string aEnd)
                     }
                     ++data_antigens[std::make_tuple(vtl, lab, date, continent)];
                     ++data_antigens[std::make_tuple(vtl, lab, year, continent)];
-                    ++data_antigens[std::make_tuple(vtl, lab, date, "")];
-                    ++data_antigens[std::make_tuple(vtl, lab, year, "")];
-                    ++data_antigens[std::make_tuple(vtl, lab, "",   continent)];
-                    ++data_antigens[std::make_tuple(vtl, lab, "",   "")];
-                    ++data_antigens[std::make_tuple(vtl, "",  date, continent)];
-                    ++data_antigens[std::make_tuple(vtl, "",  year, continent)];
-                    ++data_antigens[std::make_tuple(vtl, "",  date, "")];
-                    ++data_antigens[std::make_tuple(vtl, "",  year, "")];
-                    ++data_antigens[std::make_tuple(vtl, "",  "",   continent)];
-                    ++data_antigens[std::make_tuple(vtl, "",  "",   "")];
+                    ++data_antigens[std::make_tuple(vtl, lab, date, all)];
+                    ++data_antigens[std::make_tuple(vtl, lab, year, all)];
+                    ++data_antigens[std::make_tuple(vtl, lab, all,  continent)];
+                    ++data_antigens[std::make_tuple(vtl, lab, all,  all)];
+                    ++data_antigens[std::make_tuple(vtl, all, date, continent)];
+                    ++data_antigens[std::make_tuple(vtl, all, year, continent)];
+                    ++data_antigens[std::make_tuple(vtl, all, date, all)];
+                    ++data_antigens[std::make_tuple(vtl, all, year, all)];
+                    ++data_antigens[std::make_tuple(vtl, all, all,  continent)];
+                    ++data_antigens[std::make_tuple(vtl, all, all,  all)];
                 }
 
                 min_date = std::min(min_date, date);
@@ -157,6 +163,28 @@ std::pair<data_t, data_t> scan(std::string aStart, std::string aEnd)
 
 // ----------------------------------------------------------------------
 
+std::string make_json(const data_t& data_antigens, const data_t& data_sera)
+{
+    rjson::object data{{"antigens", rjson::object{}},
+                       {"sera", rjson::object{}},
+                       {"sera_unique", rjson::object{}},
+                       {"date", rjson::string{acmacs::date_format()}}};
+
+    for (auto [key, value]: data_antigens) {
+        auto [virus_type, lab, date, continent] = key;
+        rjson::object& antigens = data["antigens"];
+        rjson::object& for_vt = antigens.get_or_add(virus_type, rjson::object{});
+        rjson::object& for_lab = for_vt.get_or_add(lab, rjson::object{});
+        rjson::object& for_date = for_lab.get_or_add(date, rjson::object{});
+        for_date.set_field(continent, rjson::integer{value});
+    }
+
+    return data.to_json_pp(1);
+
+} // make_json
+
+// ----------------------------------------------------------------------
+
 std::string get_date(std::string aDate)
 {
     std::smatch m;
@@ -166,9 +194,6 @@ std::string get_date(std::string aDate)
     throw std::runtime_error("unrecognized date " + aDate);
 
 } // get_date
-
-// ----------------------------------------------------------------------
-
 
 // ----------------------------------------------------------------------
 /// Local Variables:
