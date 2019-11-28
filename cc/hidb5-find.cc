26df44b858db45ab63aac2f98dc1a5b43c3b4ddc
Eugene Skepner <github@skepner.eu>
2019-11-28 12:44:34 +0100

    using acmacs-base/argv

--- cc/hidb5-find.cc
+++ cc/hidb5-find.cc
@@ -1,7 +1,7 @@
 #include <cstdlib>
 #include <cmath>
 
-#include "acmacs-base/argc-argv.hh"
+#include "acmacs-base/argv.hh"
 #include "acmacs-base/string.hh"
 #include "acmacs-base/enumerate.hh"
 #include "acmacs-base/filesystem.hh"
@@ -9,80 +9,83 @@
 #include "hidb-5/hidb.hh"
 #include "hidb-5/report.hh"
 
-using namespace std::string_literals;
+// ----------------------------------------------------------------------
 
-static void list_all_antigens(const hidb::HiDb& hidb, const argc_argv& args);
-static void list_all_sera(const hidb::HiDb& hidb, const argc_argv& args);
-static void list_all_tables(const hidb::HiDb& hidb, const argc_argv& args);
-static void find_antigens(const hidb::HiDb& hidb, std::string aName, const argc_argv& args);
-static void find_antigens_by_labid(const hidb::HiDb& hidb, std::string aLabId, const argc_argv& args);
-static void find_sera(const hidb::HiDb& hidb, std::string aName, const argc_argv& args);
-static void find_tables(const hidb::HiDb& hidb, std::string aName, const argc_argv& args);
-static void find(const hidb::HiDb& hidb, const argc_argv& args);
+struct Options;
+
+static void list_all_antigens(const hidb::HiDb& hidb);
+static void list_all_sera(const hidb::HiDb& hidb, const Options& opt);
+static void list_all_tables(const hidb::HiDb& hidb);
+static void find_antigens(const hidb::HiDb& hidb, std::string_view aName);
+static void find_antigens_by_labid(const hidb::HiDb& hidb, std::string_view aLabId);
+static void find_sera(const hidb::HiDb& hidb, std::string_view aName);
+static void find_tables(const hidb::HiDb& hidb, std::string_view aName);
+static void find(const hidb::HiDb& hidb, const Options& opt);
 
 // ----------------------------------------------------------------------
+
+using namespace acmacs::argv;
+struct Options : public argv
+{
+    Options(int a_argc, const char* const a_argv[], on_error on_err = on_error::exit) : argv() { parse(a_argc, a_argv, on_err); }
+
+    option<bool> find_sera{*this, 's', desc{"find sera"}};
+    option<bool> find_table{*this, 't', desc{"find table"}};
+    option<bool> first_table{*this, "first-table"};
+    option<str>  lab{*this, "lab"};
+    option<bool> find_by_lab_id{*this, "lab-id", desc{"find by lab id"}};
+    option<str>  db_dir{*this, "db-dir"};
+
+    argument<str> virus_type{*this, arg_name{"virus-type: B, H1, H3|hidb-file"}, mandatory};
+    argument<str_array> names{*this, arg_name{"name|all"}, mandatory};
+};
 
 int main(int argc, char* const argv[])
 {
     try {
-        argc_argv args(argc, argv, {
-                {"-s", false},  // find sera
-                {"-t", false},  // find table
-                {"--first-table", false},
-                {"--lab", ""},
-                {"--labid", false},  // find by lab id
-                {"--db-dir", ""},
-                {"-v", false},
-                {"--verbose", false},
-                {"-h", false},
-                {"--help", false},
-            });
-        if (args["-h"] || args["--help"] || args.number_of_arguments() < 2) {
-            throw std::runtime_error("Usage: "s + args.program() + " [options] <virus-type: B, H1, H3|hidb-file> <name|all> ...\n" + args.usage_options());
-        }
-        const bool verbose = args["-v"] || args["--verbose"];
-        hidb::setup(std::string(args["--db-dir"]), {}, verbose);
+        Options opt(argc, argv);
+        hidb::setup(opt.db_dir);
 
-        if (fs::exists(args[0]))
-            find(hidb::HiDb(std::string(args[0]), verbose), args);
+        if (fs::exists(*opt.virus_type))
+            find(hidb::HiDb(opt.virus_type), opt);
         else
-            find(hidb::get(acmacs::virus::type_subtype_t{args[0]}, report_time::yes), args);
+            find(hidb::get(acmacs::virus::type_subtype_t{string::upper(*opt.virus_type)}, report_time::yes), opt);
         return 0;
     }
     catch (std::exception& err) {
-        std::cerr << "ERROR: " << err.what() << '\n';
+        fmt::print(stderr, "ERROR: {}\n", err);
         return 1;
     }
 }
 
 // ----------------------------------------------------------------------
 
-void find(const hidb::HiDb& hidb, const argc_argv& args)
+void find(const hidb::HiDb& hidb, const Options& opt)
 {
-    if (args[1] == "all"s) {
-        if (args["-s"])
-            list_all_sera(hidb, args);
-        else if (args["-t"])
-            list_all_tables(hidb, args);
+    if (opt.names->at(0) == "all") {
+        if (opt.find_sera)
+            list_all_sera(hidb, opt);
+        else if (opt.find_table)
+            list_all_tables(hidb);
         else
-            list_all_antigens(hidb, args);
+            list_all_antigens(hidb);
     }
     else {
-        if (args["-s"])
-            find_sera(hidb, std::string{args[1]}, args);
-        else if (args["-t"])
-            find_tables(hidb, std::string{args[1]}, args);
-        else if (args["--labid"])
-            find_antigens_by_labid(hidb, std::string{args[1]}, args);
+        if (opt.find_sera)
+            find_sera(hidb, opt.names->at(0));
+        else if (opt.find_table)
+            find_tables(hidb, opt.names->at(0));
+        else if (opt.find_by_lab_id)
+            find_antigens_by_labid(hidb, opt.names->at(0));
         else
-            find_antigens(hidb, std::string{args[1]}, args);
+            find_antigens(hidb, opt.names->at(0));
     }
 
 } // find
 
 // ----------------------------------------------------------------------
 
-void find_antigens(const hidb::HiDb& hidb, std::string aName, const argc_argv& /*args*/)
+void find_antigens(const hidb::HiDb& hidb, std::string_view aName)
 {
     try {
         std::string prefix;
@@ -95,14 +98,14 @@ void find_antigens(const hidb::HiDb& hidb, std::string aName, const argc_argv& /
             report_antigen(hidb, *antigen_index.first, true, prefix);
     }
     catch (LocationNotFound& err) {
-        throw std::runtime_error("location not found: "s + err.what());
+        throw std::runtime_error(fmt::format("location not found: {}", err));
     }
 
 } // find_antigens
 
 // ----------------------------------------------------------------------
 
-void find_antigens_by_labid(const hidb::HiDb& hidb, std::string aLabId, const argc_argv& /*args*/)
+void find_antigens_by_labid(const hidb::HiDb& hidb, std::string_view aLabId)
 {
     const auto antigens = hidb.antigens()->find_labid(string::upper(aLabId));
     for (auto antigen: antigens)
@@ -112,7 +115,7 @@ void find_antigens_by_labid(const hidb::HiDb& hidb, std::string aLabId, const ar
 
 // ----------------------------------------------------------------------
 
-void find_sera(const hidb::HiDb& hidb, std::string aName, const argc_argv& /*args*/)
+void find_sera(const hidb::HiDb& hidb, std::string_view aName)
 {
     try {
         std::string prefix;
@@ -125,14 +128,14 @@ void find_sera(const hidb::HiDb& hidb, std::string aName, const argc_argv& /*arg
             report_serum(hidb, *serum_index.first, hidb::report_tables::all);
     }
     catch (LocationNotFound& err) {
-        throw std::runtime_error("location not found: "s + err.what());
+        throw std::runtime_error(fmt::format("location not found: {}", err));
     }
 
 } // find_sera
 
 // ----------------------------------------------------------------------
 
-void find_tables(const hidb::HiDb& /*hidb*/, std::string /*aName*/, const argc_argv& /*args*/)
+void find_tables(const hidb::HiDb& /*hidb*/, std::string_view /*aName*/)
 {
     throw std::runtime_error("Not implemented");
 
@@ -140,21 +143,21 @@ void find_tables(const hidb::HiDb& /*hidb*/, std::string /*aName*/, const argc_a
 
 // ----------------------------------------------------------------------
 
-void list_all_tables(const hidb::HiDb& hidb, const argc_argv& /*args*/)
+void list_all_tables(const hidb::HiDb& hidb)
 {
     auto tables = hidb.tables();
-    std::cout << "Tables: " << tables->size() << '\n';
+    fmt::print("Tables: {}\n", tables->size());
     for (auto table: *tables)
-        std::cout << table->name() << " A:" << table->number_of_antigens() << " S:" << table->number_of_sera() << '\n';
+        fmt::print("{} A:{} S:{}\n", table->name(), table->number_of_antigens(), table->number_of_sera());
 
 } // list_all_tables
 
 // ----------------------------------------------------------------------
 
-void list_all_antigens(const hidb::HiDb& hidb, const argc_argv& /*args*/)
+void list_all_antigens(const hidb::HiDb& hidb)
 {
     auto antigens = hidb.antigens();
-    std::cout << "Antigens: " << antigens->size() << '\n';
+    fmt::print("Antigens: {}\n", antigens->size());
     for (auto antigen: *antigens)
         report_antigen(hidb, dynamic_cast<const hidb::Antigen&>(*antigen), true);
 
@@ -162,24 +165,22 @@ void list_all_antigens(const hidb::HiDb& hidb, const argc_argv& /*args*/)
 
 // ----------------------------------------------------------------------
 
-void list_all_sera(const hidb::HiDb& hidb, const argc_argv& args)
+void list_all_sera(const hidb::HiDb& hidb, const Options& opt)
 {
-    const std::string lab(args["--lab"]);
-    const bool first_table = args["--first-table"];
     auto sera = hidb.sera();
     auto tables = hidb.tables();
-    auto has_lab = [tables,&lab](const auto& sr) -> bool {
+    auto has_lab = [tables,lab=*opt.lab](const auto& sr) -> bool {
                        if (lab.empty())
                            return true;
                        const auto labs = sr.labs(*tables);
                        return std::find(labs.begin(), labs.end(), lab) != labs.end();
                    };
 
-    std::cout << "Sera: " << sera->size() << '\n';
+    fmt::print("Sera: {}\n", sera->size());
     for (auto serum: *sera) {
         const auto& sr = dynamic_cast<const hidb::Serum&>(*serum);
         if (has_lab(sr))
-            report_serum(hidb, sr, first_table ? hidb::report_tables::oldest : hidb::report_tables::all);
+            report_serum(hidb, sr, opt.first_table ? hidb::report_tables::oldest : hidb::report_tables::all);
     }
 
 } // list_all_sera
